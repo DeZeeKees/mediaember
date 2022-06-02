a59458e7c13d201c307d5ebc80b23d2b1c13e1a0
DeZeeKees <87043@roc-teraa.nl>
2022-06-02 17:00:46 +0200

    upload.php database working

--- pages/upload.php
+++ pages/upload.php
@@ -1,16 +1,63 @@
 <?php
+session_start();
 $servername = "localhost";
 $username = "test_user";
 $password = "1234";
-$dbh = new PDO("mysql:host=localhost;dbname=mediaember", $username, $password);
 
-$stmt = $dbh->prepare("insert into fileindex (username, isPublic, fileName, filePath, fileSize, fileType) values(?,?,?,?,?,?)");
-        $stmt->bindParam(1, "test");
-        $stmt->bindParam(2, 1);
-        $stmt->bindParam(3, "testfile");
-        $stmt->bindParam(4, "../storage/" . "test" . "/");
-        $stmt->bindParam(5, floor(500000 / 1024));
-        $stmt->bindParam(6, ".txt");
-        $stmt->execute();
-        $result = $stmt->setFetchMode(PDO::FETCH_NUM);
+$dbh = new PDO("mysql:host=$servername;dbname=mediaember", $username, $password);
+
+$checkboxChecked = 1;
+$sessionUsername = $_SESSION['username'];
+$fileName = $_FILES["uploadInput"]["name"];
+$filePath = "../storage/" . $_SESSION['username'] . "/" . $_FILES["uploadInput"]["name"];
+$fileSize = floor($_FILES["uploadInput"]["size"] / 1024);
+$fileType = $_FILES["uploadInput"]["type"];
+$uploadDate = date("Y-m-d");
+
+
+$stmt = $dbh->prepare("insert into fileindex (username, isPublic, fileName, filePath, fileSize, fileType, uploadDate, deletionDate) values(?,?,?,?,?,?,?,?)");
+$stmt->bindParam(1, $sessionUsername);
+$stmt->bindParam(2, $checkboxChecked);
+$stmt->bindParam(3, $fileName);
+$stmt->bindParam(4, $filePath);
+$stmt->bindParam(5, $fileSize);
+$stmt->bindParam(6, $fileType);
+$stmt->bindParam(7, $uploadDate);
+$stmt->bindParam(8, $uploadDate);
+$stmt->execute();
+$result = $stmt->setFetchMode(PDO::FETCH_NUM);
+
+$target_dir = "../storage/" . $_SESSION['username'] . "/";
+$target_file = $target_dir . basename($_FILES["uploadInput"]["name"]);
+$uploadOk = 1;
+
+// Check if file already exists
+if (file_exists($target_file)) {
+echo "Sorry, file already exists.";
+$uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["uploadInput"]["size"] > 50000000000) {
+echo "Sorry, your file is too large.";
+$uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+echo "Sorry, your file was not uploaded.";
 ?>
+    <button onclick="goBack()">Continue</button>
+<?php
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["uploadInput"]["tmp_name"], $target_file)) {
+        echo "The file ". htmlspecialchars( basename( $_FILES["uploadInput"]["name"])). " has been uploaded.";
+    ?>
+        <button onclick="goBack()">Continue</button>
+    <?php
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+        
