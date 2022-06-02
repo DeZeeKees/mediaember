81618cd9bdfa7198d61110d38ef286973071fc15
DeZeeKees <87043@roc-teraa.nl>
2022-06-02 12:25:55 +0200

    edit

--- pages/upload.php
+++ pages/upload.php
@@ -6,7 +6,7 @@
 </script>
 <?php
 session_start();
-$target_dir = "../storage/" . $_SESSION['username'];
+$target_dir = "../storage/" . $_SESSION['username'] . "/";
 $target_file = $target_dir . basename($_FILES["uploadInput"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
