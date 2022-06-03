5422816bcef03560b018c2c95ffa8e4509e2a595
DeZeeKees <87043@roc-teraa.nl>
2022-06-03 08:27:43 +0200

    ciel

--- pages/upload.php
+++ pages/upload.php
@@ -21,10 +21,9 @@ $checkboxChecked = 1;
 $sessionUsername = $_SESSION['username'];
 $fileName = $_FILES["uploadInput"]["name"];
 $filePath = "../storage/" . $_SESSION['username'] . "/" . $_FILES["uploadInput"]["name"];
-$fileSize = floor($_FILES["uploadInput"]["size"] / 1024);
+$fileSize = ceil($_FILES["uploadInput"]["size"] / 1*KB);
 $fileType = $_FILES["uploadInput"]["type"];
 $uploadDate = date("Y-m-d");
-
 
 $stmt = $dbh->prepare("insert into fileindex (username, isPublic, fileName, filePath, fileSize, fileType, uploadDate, deletionDate) values(?,?,?,?,?,?,?,?)");
 $stmt->bindParam(1, $sessionUsername);
@@ -38,40 +37,44 @@ $stmt->bindParam(8, $uploadDate);
 $stmt->execute();
 $result = $stmt->setFetchMode(PDO::FETCH_NUM);
 
-$target_dir = "../storage/" . $_SESSION['username'] . "/";
-$target_file = $target_dir . basename($_FILES["uploadInput"]["name"]);
-$uploadOk = 1;
-
-// Check if file already exists
-if (file_exists($target_file)) {
-echo "Sorry, file already exists.";
-$uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["uploadInput"]["size"] > 2*GB) {
-echo "Sorry, your file is too large.";
-$uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-echo "Sorry, your file was not uploaded.";
-?>
-    <button onclick="goBack()">Continue</button>
-<?php
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["uploadInput"]["tmp_name"], $target_file)) {
-        echo "The file ". htmlspecialchars( basename( $_FILES["uploadInput"]["name"])). " has been uploaded.";
+if($result)
+{
+    $target_dir = "../storage/" . $_SESSION['username'] . "/";
+    $target_file = $target_dir . basename($_FILES["uploadInput"]["name"]);
+    $uploadOk = 1;
+    
+    // Check if file already exists
+    if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+    }
+    
+    // Check file size
+    if ($_FILES["uploadInput"]["size"] > 0.2*GB) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+    }
+    
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
     ?>
         <button onclick="goBack()">Continue</button>
     <?php
+    // if everything is ok, try to upload file
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        if (move_uploaded_file($_FILES["uploadInput"]["tmp_name"], $target_file)) {
+            echo "The file ". htmlspecialchars( basename( $_FILES["uploadInput"]["name"])). " has been uploaded.";
         ?>
             <button onclick="goBack()">Continue</button>
         <?php
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+            ?>
+                <button onclick="goBack()">Continue</button>
+            <?php
+        }
     }
 }
+
 // h    
