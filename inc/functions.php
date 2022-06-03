4f7b57db7bb19adf167271ef2e752f10461af6e9
DeZeeKees <87043@roc-teraa.nl>
2022-06-03 10:08:59 +0200

    edit

--- inc/functions.php
+++ inc/functions.php
@@ -272,6 +272,7 @@ function registerForm()
 
     function deleteItem()
     {
+        echo "test1";
         if(isset($_REQUEST['deleteButton']))
         {
             // $servername = "localhost";
@@ -279,7 +280,7 @@ function registerForm()
             // $password = "1234";
             // $pdo = new PDO("mysql:host=$servername;dbname=mediaember", $username, $password);
             // $pdo->prepare("DELETE FROM fileIndex WHERE id=?")->execute([$id]);
-            echo "test";
+            echo "test2";
         }
     }
     ?>
