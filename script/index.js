30d43c6a3c5d42912eef2f97dc18786a1e545c1b
DeZeeKees <87043@roc-teraa.nl>
2022-06-03 10:10:53 +0200

    button change

--- script/index.js
+++ script/index.js
@@ -70,25 +70,32 @@ $(".closeUploadScreen").click(function () {
     isUploadOpen = false;
     $(".uploadDiv").hide(1000)
 });
-
-if (file.files.length == 0) {
-    $(document).on('submit', 'form', function(e){
-        e.preventDefault();
-    
-    $('.privateForm').submit(function (evt) {
-        evt.preventDefault();
-        window.history.back();
-    });
-    
-    Swal.fire({
-        icon: 'error',
-        title: 'No file selected',
-        text: 'To upload a file please select one first'
-    });
+$(".uploadSubmit").click(function(){
+    if (file.files.length === 0) {
+        $(document).on('submit', 'form', function(e){
+            e.preventDefault();
+            r
+        });
     }
+});
+// if (file.files.length == 0) {
+//     $(document).on('submit', 'form', function(e){
+//         e.preventDefault();
+    
+//     $('.privateForm').submit(function (evt) {
+//         evt.preventDefault();
+//         window.history.back();
+//     });
+    
+//     Swal.fire({
+//         icon: 'error',
+//         title: 'No file selected',
+//         text: 'To upload a file please select one first'
+//     });
+//     }
 
     
-};
+// };
 
 
 //excluding file extentions so they cany be uploaded
