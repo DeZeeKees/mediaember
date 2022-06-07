1fadb0d2a92958cbcfdceaf9d27a8678b430627d
DeZeeKees <87043@roc-teraa.nl>
2022-06-07 10:20:04 +0200

    edit

--- inc/functions.php
+++ inc/functions.php
@@ -284,7 +284,7 @@ function registerForm()
             // $password = "1234";
             // $pdo = new PDO("mysql:host=$servername;dbname=mediaember", $username, $password);
             // $pdo->prepare("DELETE FROM fileIndex WHERE id=?")->execute([$id]);
-            header("Location: ./private.php");
+            echo "<script>window.location.href = 'private.php'</script>"
         }
     }
     ?>
