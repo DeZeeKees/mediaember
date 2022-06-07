3169f14478514a2f81fcbbbbe7fc0eaf7c71d8f6
DeZeeKees <87043@roc-teraa.nl>
2022-06-07 13:22:25 +0200

    password debug

--- inc/functions.php
+++ inc/functions.php
@@ -119,8 +119,8 @@ function registerForm()
 
             $username = $_POST['usernameRegister'];
             $email = $_POST['emailRegister'];
-            $password = strval($_POST['passwordRegister']); //md5($_POST['passwordRegister']);
-            $cPassword = strval($_POST['passwordRegister']); //md5($_POST['cPasswordRegister']);
+            $password = "1234"; //md5($_POST['passwordRegister']);
+            $cPassword = "1234"; //md5($_POST['cPasswordRegister']);
             $baseCredits = 10;
             try {
                 if ($password == $cPassword) {
