c3db5a6ddae41553936ea4b5323c660e4cc1e998
DeZeeKees <87043@roc-teraa.nl>
2022-06-07 10:58:00 +0200

    edit

--- inc/functions.php
+++ inc/functions.php
@@ -280,41 +280,33 @@ function registerForm()
         {
             if($_GET['delete'] == true)
             {
-                unlink('../storage/test/heh.jpg');
+                // echo "<script> console.log('test" . $_GET['id'] ."'); </script>";
+
+                 $servername = "localhost";
+                 $username = "test_user";
+                 $password = "1234";
+                 $pdo = new PDO("mysql:host=$servername;dbname=mediaember", $username, $password);
+                
+                // $id = $_GET['id'];
+                // $stmt = $pdo->prepare( "DELETE FROM fileindex WHERE ID =:id" );
+                // $stmt->bindParam(':id', $id);
+                // $stmt->execute();
+                // $result = $stmt->setFetchMode(PDO::FETCH_NUM);
+
+                // if($result)
+                // {
+                    $stmt = $pdo->query("SELECT * FROM fileindex WHERE ID =" . $_GET['id'] . "");
+                    while ($row = $stmt->fetch()) 
+                    {
+                        echo $row['filePath'];
+                    }
+                    //echo "<script>window.location.href = 'private.php'</script>";
+                // }
+                // else
+                // {
+                //     echo "<script>console.log('Delete Error Database')</script>";
+                // }
             }  
         }
     }
-
-
-
-
-    // echo "<script> console.log('test" . $_GET['id'] ."'); </script>";
-
-    //             $servername = "localhost";
-    //             $username = "test_user";
-    //             $password = "1234";
-    //             $pdo = new PDO("mysql:host=$servername;dbname=mediaember", $username, $password);
-                
-    //             $id = $_GET['id'];
-    //             $stmt = $pdo->prepare( "DELETE FROM fileindex WHERE ID =:id" );
-    //             $stmt->bindParam(':id', $id);
-    //             $stmt->execute();
-    //             $result = $stmt->setFetchMode(PDO::FETCH_NUM);
-
-    //             if($result)
-    //             {
-    //                 $stmt = $pdo->query("SELECT * FROM fileindex WHERE ID =" . $_GET['id'] . "");
-    //                 while ($row = $stmt->fetch()) 
-    //                 {
-    //                     unlink($row['filePath']);
-    //                 }
-    //                 echo "<script>window.location.href = 'private.php'</script>";
-    //             }
-    //             else
-    //             {
-    //                 echo "<script>console.log('Delete Error Database')</script>";
-    //             }
-
-
-
     ?>
