97f769e1aba3f72ff969c9ef582c81432659e89e
DeZeeKees <87043@roc-teraa.nl>
2022-06-09 09:29:30 +0200

    edit

--- pages/private.php
+++ pages/private.php
@@ -24,16 +24,16 @@ html();
                 <p>Base Amount is 10 days</p>
                 <div class="dateInputDiv">
                     <select id="dateSelect">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
+                        <option value="1">+ 1 day</option>
+                        <option value="2">+ 2 days</option>
+                        <option value="3">+ 3 days</option>
+                        <option value="4">+ 4 days</option>
+                        <option value="5">+ 5 days</option>
+                        <option value="6">+ 6 days</option>
+                        <option value="7">+ 7 days</option>
+                        <option value="8">+ 8 days</option>
+                        <option value="9">+ 9 days</option>
+                        <option value="10">+ 10 days</option>
                     </select>
                 </div>
                 <div id="inputDiv">
