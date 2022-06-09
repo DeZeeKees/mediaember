7edd341276471775e37fc17ba0f9f99d0afe0b92
DeZeeKees <87043@roc-teraa.nl>
2022-06-09 10:42:40 +0200

    edit

--- pages/upload.php
+++ pages/upload.php
@@ -48,42 +48,38 @@ $result = $stmt->setFetchMode(PDO::FETCH_NUM);
 
 if($result)
 {
-    $target_dir = "../storage/" . $_SESSION['username'] . "/";
-    $target_file = $target_dir . basename($_FILES["uploadInput"]["name"]);
-    $uploadOk = 1;
     
-    // Check if file already exists
-    if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-    }
-    
-    // Check file size
-    if ($_FILES["uploadInput"]["size"] > 50000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-    }
-    
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-    ?>
-        <script>Error()</script>
-    <?php
-    // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["uploadInput"]["tmp_name"], $target_file)) {
-            echo "The file ". htmlspecialchars( basename( $_FILES["uploadInput"]["name"])). " has been uploaded.";
-        ?>
-            <script>goBack()</script>
-        <?php
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-            ?>
-                <script>Error()</script>
-            <?php
-        }
-    }
 }
 
-// h    
+// $target_dir = "../storage/" . $_SESSION['username'] . "/";
+//     $target_file = $target_dir . basename($_FILES["uploadInput"]["name"]);
+//     $uploadOk = 1;
+    
+//     // Check if file already exists
+//     if (file_exists($target_file)) {
+//     $uploadOk = 0;
+//     }
+    
+//     // Check file size
+//     if ($_FILES["uploadInput"]["size"] > 50000000) {
+//     $uploadOk = 0;
+//     }
+    
+//     // Check if $uploadOk is set to 0 by an error
+//     if ($uploadOk == 0) {
+//     ?>
+//         <script>Error()</script>
+//     <?php
+//     // if everything is ok, try to upload file
+//     } else {
+//         if (move_uploaded_file($_FILES["uploadInput"]["tmp_name"], $target_file)) {
+//             echo "The file ". htmlspecialchars( basename( $_FILES["uploadInput"]["name"])). " has been uploaded.";
+//         ?>
+//             <script>goBack()</script>
+//         <?php
+//         } else {
+//             ?>
+//                 <script>Error()</script>
+//             <?php
+//         }
+//     }
