67f3999d783ab9a59e1b1607af670911f06eae57
DeZeeKees <87043@roc-teraa.nl>
2022-06-15 10:54:17 +0200

    edit

--- bot/deleteExpired.php
+++ bot/deleteExpired.php
@@ -1,27 +1,17 @@
 <?php
 
-function unlinkItems()
+$servername = "localhost";
+$username = "test_user";
+$password = "1234";
+$dbh = new PDO("mysql:host=$servername;dbname=mediaember", $username, $password);
+$stmt1 = $dbh->query("SELECT * FROM `fileindex` WHERE deletionDate < NOW()");
+while ($row = $stmt1->fetch()) 
 {
-    $servername = "localhost";
-    $username = "test_user";
-    $password = "1234";
-    $dbh = new PDO("mysql:host=$servername;dbname=mediaember", $username, $password);
-    $stmt1 = $dbh->query("SELECT * FROM `fileindex` WHERE deletionDate < NOW()");
-    while ($row = $stmt1->fetch()) 
-    {
-        unlink($row['filePath']);
-    }
-    return true;
+    unlink($row['filePath']);
 }
 
-if(unlinkItems())
-{
-    $servername = "localhost";
-    $username = "test_user";
-    $password = "1234";
-    $dbh = new PDO("mysql:host=$servername;dbname=mediaember", $username, $password);
-    $stmt = $dbh->prepare("SELECT * FROM `fileindex` WHERE deletionDate < NOW()");
-    $stmt->execute();
-    $result2 = $stmt->setFetchMode(PDO::FETCH_NUM);
-}
+$dbh = new PDO("mysql:host=$servername;dbname=mediaember", $username, $password);
+$stmt = $dbh->prepare("SELECT * FROM `fileindex` WHERE deletionDate < NOW()");
+$stmt->execute();
+
 
