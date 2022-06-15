72619c1af1c146ff01e5bbdb77b242d019a88767
DeZeeKees <87043@roc-teraa.nl>
2022-06-15 10:57:22 +0200

    Update deleteExpired.php

--- bot/deleteExpired.php
+++ bot/deleteExpired.php
@@ -20,7 +20,7 @@ if(unlinkItems())
     $username = "test_user";
     $password = "1234";
     $dbh = new PDO("mysql:host=$servername;dbname=mediaember", $username, $password);
-    $stmt = $dbh->prepare("DELETE * FROM `fileindex` WHERE deletionDate < NOW()");
+    $stmt = $dbh->prepare("DELETE FROM `fileindex` WHERE deletionDate < NOW()");
     $stmt->execute();
     $result2 = $stmt->setFetchMode(PDO::FETCH_NUM);
 }
