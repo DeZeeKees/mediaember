1a784a22fbae898475bc043491eb18693972082b
Lobster <87379@roc-teraa.nl>
2022-06-17 10:40:55 +0200

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -297,7 +297,7 @@ function registerForm()
                          if(date("Y-m-d", strtotime($row["deletionDate"] . " - " . 1 . " days")) == date("Y-m-d"))
                          {
                             ?> 
-                                <p><span class='material-symbols-outlined iconsize'>error</span></p>
+                                <p><span class='material-symbols-outlined'>error</span></p>
                         <?php
                           }
                         ?>                     
