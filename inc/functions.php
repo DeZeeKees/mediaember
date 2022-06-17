785dd07e3e732f14cc0dfc988b71946244768f52
Lobster <87379@roc-teraa.nl>
2022-06-17 10:38:38 +0200

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -294,7 +294,7 @@ function registerForm()
                 ?>
                     <div class="publicItem">
                          <?php
-                         if(date("Y-m-d") ==date("Y-m-d", strtotime($row["deletionDate"], null ,"Y-m-d" . " - " . 1 . " days")))
+                         if(date("Y-m-d", strtotime($row["deletionDate"] ,"Y-m-d" . " - " . 1 . " days")) == date("Y-m-d"))
                          {
                             ?> 
                                 <p><span class='material-symbols-outlined iconsize'>error</span></p>
